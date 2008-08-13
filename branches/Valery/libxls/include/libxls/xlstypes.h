35a4a0132d905bea2cfa07954b9eefe28f1ca596
cleit <cleit@982eeb1d-6a45-41fa-94c8-6813189e3c3d>
2008-08-13 01:13:49 +0000

    Removed unused "Valery" branch

--- branches/Valery/libxls/include/libxls/xlstypes.h
+++
@@ -1,5 +0,0 @@
-#pragma pack(1)
-typedef unsigned char           BYTE;//  __attribute__ ((aligned (1)));	// 1 bytes
-typedef unsigned short          WORD;// __attribute__ ((aligned (1)));	// 2 bytes
-typedef unsigned long           DWORD;//  __attribute__ ((aligned (1)));	// 4 bytes
-//typedef unsigned long long      ULLONG;// __attribute__ ((aligned (1)));	// 8 bytes
